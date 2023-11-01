a00684b884ebbc5b4971c49cea9f75d4083a472c
hamsm <hamsm3184@naver.com>
2023-11-01 16:43:25 +0900

    update

---
+++ src/week9/version02/FindGuitarTester.java
@@ -0,0 +1,42 @@
+package week9.version02;
+
+import java.util.Iterator;
+import java. util.List;
+
+public class FindGuitarTester {
+
+    public static void main(String[] args) {
+        Inventory inventory = new Inventory();
+        initializeInventory(inventory);
+
+        Guitar whatErinLikes = new Guitar("", 0, Builder.FENDER, "Stratocastor", Type.ELECTRIC,
+                Wood.ALDER, Wood.ALDER);
+
+        List<Guitar> matchingGuitars = inventory.search(whatErinLikes);
+
+        if(!matchingGuitars.isEmpty()) {
+
+            System.out.println("Erin, you like these guitars:");
+
+            for(Iterator<Guitar> i = matchingGuitars.iterator(); i.hasNext();) {
+                Guitar guitar = (Guitar) i.next();
+                System.out.println(" We have a " + guitar.getBuilder() + " "
+                        + guitar.getModel() + " "
+                        + guitar.getType() + " guitar:\n     "
+                        + guitar.getBackWood() + " back and sides,\n     "
+                        + guitar.getTopWood() + " top.\n "
+                        +"You can have it for only $" + guitar.getPrice()
+                        + "!\n ----");
+            }
+        } else {
+            System.out.println("Sorry, Erin, we have nothing for you.");
+        }
+    }
+
+    private static void initializeInventory(Inventory inventory) {
+        inventory.addGuitar("11277", 3999.95, Builder.COLLINGS, "CJ", Type.ACOUSTIC, Wood.INDIAN_ROSEWOOD, Wood.SITKA);
+        inventory.addGuitar("V9563", 1499.95, Builder.FENDER, "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+        inventory.addGuitar("V9512", 1549.95, Builder.FENDER, "Stratocastor", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+    }
+
+}
---
+++ src/week9/version02/Guitar.java
@@ -0,0 +1,53 @@
+package week9.version02;
+
+public class Guitar {
+    private String serialNumber, model;
+    private Builder builder;
+    private Type type;
+    private Wood backWood, topWood;
+    private double price;
+
+    public Guitar(String serialNumber, double price, Builder builder, String model, Type type, Wood backWood, Wood topWood) {
+        this.serialNumber = serialNumber;
+        this.builder = builder;
+        this.model = model;
+        this.type = type;
+        this.backWood = backWood;
+        this.topWood = topWood;
+        this.price = price;
+    }
+
+// getter, setter
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Builder getBuilder() {
+        return builder;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Wood getBackWood() {
+        return backWood;
+    }
+
+    public Wood getTopWood() {
+        return topWood;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
